2bf6606e1469aa283b93ebb6b396e978ccbe9e3d
Clint Troxel <clint@ctro.net>
2014-01-17 16:59:49 -0700

    fix paperclip?

--- config/environment.rb
+++ config/environment.rb
@@ -6,17 +6,6 @@ Chasingtails::Application.initialize!
 
 # Configure all environments
 Chasingtails::Application.configure do
-
-	# paperclip/aws
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-  
 end
 
 ActionMailer::Base.smtp_settings = {
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Chasingtails::Application.configure do
 
   # Mail
   config.action_mailer.delivery_method       = :sendmail
-  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.perform_deliveries    = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options   = { :host => 'localhost:3000' }
 
