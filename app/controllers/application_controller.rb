b41086ea32cadb047163f12d3ac42cda76895e1d
Clint Troxel <clint@ctro.net>
2014-01-17 12:07:46 -0700

    some better auth stuff (forgot pw emails, logout works, etc)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
 
 	private
   def after_sign_out_path_for(resource_or_scope)
-  	redirect_to new_user_session_path
+  	new_user_session_path
 	end
 end
