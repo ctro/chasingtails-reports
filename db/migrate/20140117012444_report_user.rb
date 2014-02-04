81525488dfa578f8f52df454c5726d32d47b399a
Clay Preheim <clay.preheim@gmail.com>
2014-02-04 16:59:46 -0700

    style updates

--- db/migrate/20140117012444_report_user.rb
+++ db/migrate/20140117012444_report_user.rb
@@ -1,6 +1,6 @@
 class ReportUser < ActiveRecord::Migration
   def change
   	add_column :reports, :user_id, :integer
-  	Report.update_all(:user_id => User.first.id)
+    # Report.update_all(:user_id => User.first.id)
   end
 end
