d7535f18ad9607fb96a5850075c58b12876f0cbd
Clint Troxel <clint@ctro.net>
2014-02-06 19:16:50 -0700

    User (Pet care provider) names

--- db/migrate/20140117183134_initial_admin_user.rb
+++ db/migrate/20140117183134_initial_admin_user.rb
@@ -1,6 +1,6 @@
 class InitialAdminUser < ActiveRecord::Migration
   def change
   	pw = "fredrickdouglass"
-  	User.create(:admin => true, :email => "info@chasingtailsjh.com", :password => pw, :password_confirmation => pw)
+  	User.create(:admin => true, :name => "Fredrick Douglass III", :email => "info@chasingtailsjh.com", :password => pw, :password_confirmation => pw)
   end
 end
