a703a5606f7bcb37b9f83321d4c49f206d6742e8
ctro <clint@ctro.net>
2019-03-14 20:01:54 -0600

    solve another way

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -3,6 +3,6 @@ module ClientsHelper
   def dog_links(client)
     client.dogs.map do |d|
       link_to d.name, client_dog_path(client, d)
-    end.join(', ')
+    end.join(', ').html_safe
   end
 end
